4529d011ce4bcee40d13e8f3646265b7ffe09356
Julián <julian@unity3d.com>
2020-09-07 21:49:49 -0500

    added toast

--- app/src/main/java/com/umb/openglandroid/OpenGLDemoActivity.java
+++ app/src/main/java/com/umb/openglandroid/OpenGLDemoActivity.java
@@ -1,12 +1,14 @@
 package com.umb.openglandroid;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 import android.opengl.GLSurfaceView;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class OpenGLDemoActivity extends Activity {
 
@@ -26,5 +28,10 @@ public class OpenGLDemoActivity extends Activity {
         EditText editBox = new EditText(this);
         editBox.setText("Hello World: The Spinning Cube at OpenGLES 2.0");
         addContentView(editBox, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
+        Context context = getApplicationContext();
+        String msg = getApplicationInfo().packageName;
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT * 5).show();
     }
 }
